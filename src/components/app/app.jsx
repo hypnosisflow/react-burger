5387888eed6b5d484b081bb22abd9d5be6da6d62
Artem <plzv92@gmail.com>
2022-09-13 12:24:27 +0700

    Fixed names

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,7 +3,7 @@ import styles from './app.module.css';
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
-import Modal from "../Modal/modal";
+import Modal from "../modal/modal";
 import IngredientsDetails from "../ingredients-details/ingredients-details";
 import OrderDetails from "../order-details/order-details";
 
